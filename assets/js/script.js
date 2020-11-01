0274c81b24c334f75ad0fe701b473f39b2b506ff
Thor-Ole Øby <punkthor@gmail.com>
2020-11-01 19:28:14 +0100

    Testing out different apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,9 @@ $(".col-sm-3").click(function () {
 
 function changeBackground() {
     let boxes = $(".col-sm-3");
-    let colors = ["blue", "green", "yellow"]
+    let colors = ["#037971", "#037971", "#03b5aa"]
     for (let index = 0; index < boxes.length; index++) {
-        $(boxes[index]).css("background", colors[index])
+        $(boxes[index]).css("background", colors[Math.floor(Math.random() * colors.length)])
     }
 }
 
@@ -56,4 +56,73 @@ function createButtons(someJson) {
         $(button).text(someJson.results[index].name)
         $("#ships").append(button)
     }
+}
+
+/*
+Geolocation
+*/
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+    else {
+        $("col-12 p").text("Geolocation is not supported by your browser");
+    }
+}
+
+
+function showPosition(position) {
+    let newLine = document.createElement("br")
+    $(".col-12 p").append(position.coords.latitude, newLine, position.coords.longitude); 
+}
+
+
+/*
+Map
+*/
+function showMyPosition(position) {
+    let myLat = position.coords.latitude;
+    let myLng = position.coords.longitude
+    let mapProp = {
+        center:new google.maps.LatLng(myLat, myLng),
+        zoom:15,
+    };
+ 
+    let map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+}
+
+
+function myMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMyPosition)
+    }
+}
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                $(".contact-heading").text("Thank You!");
+                $(".contact-sub").text("I'll be back to you in no time!")
+                resetForm();
+                setTimeout(function () {
+                    $(".contact-heading").text("Got a project in mind?");
+                    $(".contact-sub").text("Let's work together!")  
+                }, 8000)
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+}
+
+function resetForm() {
+    $("input, textarea").val("");
 }
